493d80ece4988a1c662f63fc343f5cd3e726d90e
Pushkar Mondal <nishitm060@gmail.com>
2026-03-11 15:23:15 +0530

    Graph nodes connected using neo4j

---
+++ server/src/services/graph-neo4j.service.ts
@@ -0,0 +1,58 @@
+import { getSession } from "../../db/neo4j"
+
+export const createUserNode = async (id: string, name: string, email: string) => {
+  const session = getSession();
+  await session.run(
+    `
+    CREATE (u:User {
+    id: $id,
+    name: $name,
+    email: $email
+    })
+    `,
+    { id, name, email }
+  )
+  await session.close();
+}
+
+export const createCompanyNode = async (id: string, name: string, industry: string) => {
+  const session = getSession()
+  await session.run(
+    `
+    CREATE (c:Company {
+    id: $id,
+    name: $name,
+    industry: $industry
+    })
+    `,
+    { id, name, industry }
+  )
+  await session.close();
+}
+
+export const createSkillNode = async (id: string, name: string) => {
+  const session = getSession();
+  await session.run(
+    `
+    CREATE (s:Skill {
+    id: $id,
+    name: $name
+    })
+    `,
+    { id, name }
+  )
+  await session.close();
+}
+
+export const attachCompanyToUser = async (userId: string, companyId: string) => {
+  const session = getSession();
+  await session.run(
+    `
+    MATCH (u:User {id: $userId})
+    MATCH (c:Company {id: $companyId})
+    MERGE (u)-[:WORKS_AT]->(c)
+    `,
+    {userId, companyId}
+  )
+}
+
